736cb389000541627cdc575118ad9fa089c3676a
Duong Vong <duongvongmt02@gamil.com>
2018-12-05 16:21:14 +0700

    login and logout success

--- Face-Recognition-On-Rasp/server/src/server.js
+++ Face-Recognition-On-Rasp/server/src/server.js
@@ -1,55 +1,65 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+  var express = require('express');
+  var app = express();
+  var bodyParser = require('body-parser');
 
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+  var MongoClient = require('mongodb').MongoClient;
+  var url = "mongodb://localhost:27017/";
 
+  var checklogin=false;
 
-
-function connectMongo(callback){
-	MongoClient.connect(url, function(err, db) {
-	  if (err) throw err;
-	  var dbo = db.db("rapbery");
-	  dbo.collection("users").find({}).toArray(function(err, result) {
-	    if (err) throw err;
-	    // console.log(result[0].username);
-	    db.close();
-	    callback(result);
-	  });
-	});	
-}
+  function dataArrayMongo(callback,db_name,collection_name){
+  	MongoClient.connect(url, function(err, db) {
+  	  if (err) throw err;
+  	  var dbo = db.db(db_name);
+  	  dbo.collection(collection_name).find({}).toArray(function(err, result) {
+  	    if (err) throw err;
+  	    db.close();
+  	    callback(result);
+  	  });
+  	});	
+  }
 // Tao mot parser co dang application/x-www-form-urlencoded
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+  var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-app.use(express.static('public'));
+  app.use(express.static('public'));
 
-app.post('/check_login', urlencodedParser, function (req, res) {
-   // console.log(req.body.first_name);
-   // console.log(req.body.last_name);
-   connectMongo(function(result){
-   	console.log(result);	
-   })
-   
-   // if(req.body.first_name==result[0].username && req.body.last_name==result[0].password){
-   // 	return res.redirect('/login');
-   // }
-})
+  app.post('/check_login', urlencodedParser, function (req, res) {
+     db_name="rapbery";
+     collection_name="users";
+     dataArrayMongo(function(result){ 
+        let leng=result.length;
+        let i;
+        for(i=0;i<leng;i++){
+          if(req.body.username==result[i].username && req.body.password==result[i].password){
+            checklogin=true;
+            return res.redirect('/home');
+          }
+        }
+        return res.redirect('/')
+     },db_name,collection_name);
+  })
 
-app.get('/login',function(req,res){
-	res.send('Hello world');
-})
+  app.get('/home',function(req,res){
+  	if(checklogin) {
+      res.sendFile(__dirname + "/" + "home.html");
+    }
+    else res.redirect('/');
+  })
+  
+  app.post('/logout',function(req,res){
+    checklogin=false;
+    return res.redirect('/');
+  })
+  app.get('/', function (req, res) {
+  	res.sendFile( __dirname + "/" + "index.html" );
+  })
 
-app.get('/', function (req, res) {
-	res.sendFile( __dirname + "/" + "index.html" );
-})
-
-var server = app.listen(80, function () {
+  var server = app.listen(80, function () {
 
   var host = server.address().address
   var port = server.address().port
 
   console.log("Ung dung Node.js dang lang nghe tai dia chi: http://%s:%s", host, port)
 
-})
+  })
 
