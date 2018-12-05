36553bc31fdf1647d468b18f9abe701e4b2d8aad
ad0min <boss1905@outlook.com>
2018-12-05 19:25:34 +0700

    [Update] test video

--- Face-Recognition-On-Rasp/gateway/pi-face-recognition/test_cam.py
+++ Face-Recognition-On-Rasp/gateway/pi-face-recognition/test_cam.py
@@ -3,10 +3,8 @@ from time import sleep
 
 camera = PiCamera()
 
-
 camera.start_preview()
-for i in range(5):
-    sleep(5)
-    camera.capture('/home/pi/Desktop/image%s.jpg' % i)
+camera.start_recording('/home/pi/video.h264')
+sleep(10)
+camera.stop_recording()
 camera.stop_preview()
-
