f1409a73901ee2993d5d911f6c08e0137f8688e5
Tran Quoc Khanh <khanhtran.cse@gmail.com>
2018-12-05 18:43:55 +0700

    "Move"

--- Face-Recognition-On-Rasp/server/src/server.js
+++ Face-Recognition-On-Rasp/server/src/server.js
@@ -1,95 +1,115 @@
-<<<<<<< HEAD
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+var url = "mongodb://192.168.1.23:27017/";
 
-function connectMongo(callback){
-	MongoClient.connect(url, function(err, db) {
-	  if (err) throw err;
-	  var dbo = db.db("rapbery");
-	  dbo.collection("users").find({}).toArray(function(err, result) {
-	    if (err) throw err;
-	    // console.log(result[0].username);
-	    db.close();
-	    callback(result);
-	  });
-	});	
+var checklogin = false;
+
+function dataArrayMongo(callback, db_name, collection_name) {
+	MongoClient.connect(url, function (err, db) {
+		if (err) throw err;
+		var dbo = db.db(db_name);
+		dbo.collection(collection_name).find({}).toArray(function (err, result) {
+			if (err) throw err;
+			db.close();
+			callback(result);
+		});
+	});
 }
 // Tao mot parser co dang application/x-www-form-urlencoded
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
-=======
-  var express = require('express');
-  var app = express();
-  var bodyParser = require('body-parser');
 
-  var MongoClient = require('mongodb').MongoClient;
-  var url = "mongodb://localhost:27017/";
->>>>>>> 8c1d2ae8b4431159a5ee62dd5fb4cae3070e1ac1
+app.use(express.static('public'));
+app.set('view engine', 'ejs')
 
-  var checklogin=false;
-
-  function dataArrayMongo(callback,db_name,collection_name){
-  	MongoClient.connect(url, function(err, db) {
-  	  if (err) throw err;
-  	  var dbo = db.db(db_name);
-  	  dbo.collection(collection_name).find({}).toArray(function(err, result) {
-  	    if (err) throw err;
-  	    db.close();
-  	    callback(result);
-  	  });
-  	});	
-  }
-// Tao mot parser co dang application/x-www-form-urlencoded
-  var urlencodedParser = bodyParser.urlencoded({ extended: false })
-
-  app.use(express.static('public'));
-
-  app.post('/check_login', urlencodedParser, function (req, res) {
-     db_name="rapbery";
-     collection_name="users";
-     dataArrayMongo(function(result){ 
-        let leng=result.length;
-        let i;
-        for(i=0;i<leng;i++){
-          if(req.body.username==result[i].username && req.body.password==result[i].password){
-            checklogin=true;
-            return res.redirect('/home');
-          }
-        }
-        return res.redirect('/')
-     },db_name,collection_name);
-  })
-
-<<<<<<< HEAD
-app.get('/', function (req, res) {
-	res.sendFile( __dirname + "/view/index.html" );
+app.post('/check_login', urlencodedParser, function (req, res) {
+	db_name = "rapbery";
+	collection_name = "users";
+	dataArrayMongo(function (result) {
+		let leng = result.length;
+		let i;
+		for (i = 0; i < leng; i++) {
+			if (req.body.username == result[i].username && req.body.password == result[i].password) {
+				checklogin = true;
+				return res.redirect('/home');
+			}
+		}
+		return res.redirect('/')
+	}, db_name, collection_name);
 })
-=======
-  app.get('/home',function(req,res){
-  	if(checklogin) {
-      res.sendFile(__dirname + "/" + "home.html");
-    }
-    else res.redirect('/');
-  })
-  
-  app.post('/logout',function(req,res){
-    checklogin=false;
-    return res.redirect('/');
-  })
-  app.get('/', function (req, res) {
-  	res.sendFile( __dirname + "/" + "index.html" );
-  })
->>>>>>> 8c1d2ae8b4431159a5ee62dd5fb4cae3070e1ac1
 
-  var server = app.listen(80, function () {
 
-  var host = server.address().address
-  var port = server.address().port
+app.get('/home', function (req, res) {
+	res.sendFile(__dirname + "/views/home.html");
+})
 
-  console.log("Ung dung Node.js dang lang nghe tai dia chi: http://%s:%s", host, port)
+app.get('/log',function(req,res){
+	const log = [
+		{
+			date: "04/12/2018",
+			time: "12:03:00",
+			username: "Khanh Tran",
+			userId: 12,
+			status: 0,
+			image: "link",
+		},
+		{
+			date: "04/12/2018",
+			time: "12:03:00",
+			username: "Khanh Tran",
+			userId: 12,
+			status: 0,
+			image: "link",
+		},
+		{
+			date: "04/12/2018",
+			time: "12:03:00",
+			username: "Khanh Tran",
+			userId: 12,
+			status: 0,
+			image: "link",
+		},
+		{
+			date: "04/12/2018",
+			time: "12:03:00",
+			username: "Khanh Tran",
+			userId: 12,
+			status: 0,
+			image: "link",
+		},{
+			date: "04/12/2018",
+			time: "12:03:00",
+			username: "Khanh Tran",
+			userId: 12,
+			status: 0,
+			image: "link",
+		}
+	]
 
-  })
+	res.render("pages/log.ejs",{logs:log});
+});
+
+app.get('/user',function(req,res){
+
+	res.sendFile(__dirname + "/views/user.html");
+});
+
+app.post('/logout', function (req, res) {
+	checklogin = false;
+	return res.redirect('/');
+})
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + "/views/index.html");
+})
+
+var server = app.listen(80, function () {
+
+	var host = server.address().address
+	var port = server.address().port
+
+	console.log("Ung dung Node.js dang lang nghe tai dia chi: http://%s:%s", host, port)
+
+})
 
