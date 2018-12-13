ead21cd3a417ad28680cbea8f2e0d9fb6f35b403
Tran Quoc Khanh <khanhtran.cse@gmail.com>
2018-12-14 02:27:30 +0700

    Update action in door

--- Face-Recognition-On-Rasp/server/server.js
+++ Face-Recognition-On-Rasp/server/server.js
@@ -14,19 +14,19 @@ app.use(session({
 	secret: 'work hard raspberry',
 	resave: true,
 	saveUninitialized: false
-  }));
+}));
 app.use(express.json());
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 
-app.use((req,res,next)=>{
-	if(req.originalUrl == '/login'){
+app.use((req, res, next) => {
+	if (req.originalUrl == '/login') {
 		next();
-	} else if(req.session.userId){
+	} else if (req.session.userId) {
 		next();
-	} else{
+	} else {
 		res.redirect('/login');
 	}
 })
@@ -34,11 +34,11 @@ app.use((req,res,next)=>{
 app.post('/login', urlencodedParser, function (req, res) {
 	console.log(req.body);
 	if ((req.body.username == 'admin' && req.body.password == 'admin') ||
-	(req.body.username == 'ad' && req.body.password == 'ad')) {
+		(req.body.username == 'ad' && req.body.password == 'ad')) {
 		const session = req.session;
 		session.userId = req.body.username;
 		return res.redirect('/home');
-	} else{
+	} else {
 		return res.redirect('/');
 	}
 })
@@ -51,7 +51,7 @@ app.get('/home', function (req, res) {
 app.get('/log', function (req, res) {
 	database.getLogs().then(result => {
 		result = result.map((item) => {
-			var date = new Date(item.timestamp);	
+			var date = new Date(item.timestamp);
 			// Hours part from the timestamp
 			var hours = date.getHours();
 			// Minutes part from the timestamp
@@ -85,7 +85,7 @@ app.post('/add-log', bodyParser.raw(), (req, res) => {
 
 app.get('/user', function (req, res) {
 	database.getPersons().then(result => {
-		console.log("render result user 1: ",result);
+		console.log("render result user 1: ", result);
 		res.render("user", { users: result });
 	})
 		.catch(err => {
@@ -145,7 +145,7 @@ app.post('/add-user', (req, res) => {
 			fields.status = parseInt(fields.status);
 			// console.log(fields);
 			database.addPerson(fields);
-			console.log("data insert user: ",fields);
+			console.log("data insert user: ", fields);
 			res.redirect('/user');
 		} catch (err) {
 			console.log(err);
@@ -153,28 +153,28 @@ app.post('/add-user', (req, res) => {
 		}
 	});
 });
-app.get('/delete-person', (req,res)=>{
+app.get('/delete-person', (req, res) => {
 	const id = req.query.id;
-	console.log('delete person',id);
-	database.deletePerson(id).then(result=>{
+	console.log('delete person', id);
+	database.deletePerson(id).then(result => {
 		console.log(result);
 		res.redirect('/user');
 	})
-	.catch(err=>{
-		console.log(err);
-		res.redirect('/user');
-	})
+		.catch(err => {
+			console.log(err);
+			res.redirect('/user');
+		})
 });
 
 
-app.get('/department',function (req,res){
+app.get('/department', function (req, res) {
 	database.getDepartment().then(result => {
 
-		res.render('department',{departments: result}); //need to edit later	
+		res.render('department', { departments: result }); //need to edit later	
 	})
-	
+
 })
-app.get('/add-department',(req,res) => {
+app.get('/add-department', (req, res) => {
 	res.render('add-department')
 })
 app.post('/add-department', (req, res) => {
@@ -188,7 +188,7 @@ app.post('/add-department', (req, res) => {
 		// console.log(files);
 		try {
 			database.addDepartment(fields);
-			console.log("data insert user: ",fields);
+			console.log("data insert user: ", fields);
 			res.redirect('/department');
 		} catch (err) {
 			console.log(err);
@@ -196,27 +196,36 @@ app.post('/add-department', (req, res) => {
 		}
 	});
 });
-app.get('/delete-department', (req,res)=>{
+app.get('/delete-department', (req, res) => {
 	const id = req.query.id;
-	console.log('delete department',id);
-	database.deleteDepartment(id).then(result=>{
+	console.log('delete department', id);
+	database.deleteDepartment(id).then(result => {
 		console.log(result);
 		res.redirect('/department');
 	})
-	.catch(err=>{
-		console.log(err);
-		res.redirect('/department');
-	})
+		.catch(err => {
+			console.log(err);
+			res.redirect('/department');
+		})
 });
 
-app.get('/door',function (req,res){
+app.get('/door', function (req, res) {
+	const params = req.query;
+	let status;
+	let message;
+	if(params.action && params.status){
+		message = params.action + ' ' + params.status;
+		status = params.status;
+	}
+
 	database.getDoor().then(result => {
-		console.log(result);
-		res.render('door',{doors: result}); //need to edit later	
+		// console.log(result);
+		res.render('door', { doors: result ,message: message,status: status,
+		doorId: params.id}); //need to edit later	
 	})
 })
 
-app.get('/add-door',function (req,res){
+app.get('/add-door', function (req, res) {
 	res.render('add-door'); //need to edit later
 })
 app.post('/add-door', (req, res) => {
@@ -225,10 +234,10 @@ app.post('/add-door', (req, res) => {
 	form.multiples = true;
 
 	form.parse(req, function (err, fields) {
-		
+
 		try {
 			database.addDoor(fields);
-			console.log("data insert user: ",fields);
+			console.log("data insert user: ", fields);
 			res.redirect('/door');
 		} catch (err) {
 			console.log(err);
@@ -236,24 +245,24 @@ app.post('/add-door', (req, res) => {
 		}
 	});
 });
-app.get('/delete-door', (req,res)=>{
+app.get('/delete-door', (req, res) => {
 	const id = req.query.id;
-	console.log('delete door',id);
-	database.deleteDoor(id).then(result=>{
+	console.log('delete door', id);
+	database.deleteDoor(id).then(result => {
 		console.log(result);
 		res.redirect('/door');
 	})
-	.catch(err=>{
-		console.log(err);
-		res.redirect('/door');
-	})
+		.catch(err => {
+			console.log(err);
+			res.redirect('/door');
+		})
 });
 
-app.get('/permission',function (req,res){
+app.get('/permission', function (req, res) {
 	res.render('permission'); //need to edit later
 })
 
-app.get('/add-permission',function (req,res){
+app.get('/add-permission', function (req, res) {
 	res.render('add-permission'); //need to edit later
 })
 
@@ -263,10 +272,10 @@ app.post('/add-permission', (req, res) => {
 	form.multiples = true;
 
 	form.parse(req, function (err, fields) {
-		
+
 		try {
 			database.addDoor(fields);
-			console.log("data insert permission: ",fields);
+			console.log("data insert permission: ", fields);
 			res.redirect('/permission');
 		} catch (err) {
 			console.log(err);
@@ -274,17 +283,17 @@ app.post('/add-permission', (req, res) => {
 		}
 	});
 });
-app.get('/delete-door', (req,res)=>{
+app.get('/delete-door', (req, res) => {
 	const id = req.query.id;
-	console.log('delete door',id);
-	database.deleteDoor(id).then(result=>{
+	console.log('delete door', id);
+	database.deleteDoor(id).then(result => {
 		console.log(result);
 		res.redirect('/door');
 	})
-	.catch(err=>{
-		console.log(err);
-		res.redirect('/door');
-	})
+		.catch(err => {
+			console.log(err);
+			res.redirect('/door');
+		})
 });
 
 app.post('/logout', function (req, res) {
@@ -299,13 +308,38 @@ app.get('/', function (req, res) {
 	res.redirect('/home');
 })
 
-app.get('/login',(req,res)=>{
+app.get('/login', (req, res) => {
 	console.log(req.session);
-	if(req.session.userId){
+	if (req.session.userId) {
 		res.redirect('/home');
-	} else{
+	} else {
 		res.render('index');
 	}
+});
+
+app.get('/door/open', (req, res) => {
+	const params = req.query;
+	const id = params["id"];
+	res.redirect('/door?action=Open door&status=success&id=' + id);
+});
+
+app.get('/door/close', (req, res) => {
+	const params = req.query;
+	const id = params["id"];
+	console.log(id);
+	res.redirect('/door?action=Close door&status=success&id=' + id);
+});
+
+app.get('/door/lock', (req, res) => {
+	const params = req.query;
+	const id = params["id"];
+	res.redirect('/door?action=Lock door&status=success&id=' + id);
+});
+
+app.get('/door/take-picture', (req, res) => {
+	const params = req.query;
+	const id = params["id"];
+	res.redirect('/door?action=Take picture&status=success&id=' + id);
 });
 
 
